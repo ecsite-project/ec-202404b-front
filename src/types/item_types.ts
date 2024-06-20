4111feb0e0011fa5d9d5e0c42308ebab6377697a
char5742 <fujino@stellarcode.jp>
2024-06-20 16:02:32 +0900

    update: バックに合わせてデータ形式を修正

--- src/types/item_types.ts
+++ src/types/item_types.ts
@@ -1,15 +1,26 @@
 import { z } from 'astro/zod';
 
+export const BreedSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+});
+
+export const ColorSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+});
+
 export const ItemSchema = z.object({
   id: z.string(),
   description: z.string(),
   price: z.number(),
-  birthDay: z.date(),
+  birthDay: z.string(),
   image: z.string(),
   gender: z.string(),
-  breed: z.string(),
-  color: z.string(),
-  weight: z.number(),
+  breed: BreedSchema,
+  color: ColorSchema,
 });
 
 export type Item = z.infer<typeof ItemSchema>;
+
+export const ItemListSchema = z.array(ItemSchema);
