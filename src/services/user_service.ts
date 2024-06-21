d653300f62d03c8374409ab7b8fe4c0ce3100539
udonrm <b201395@hiroshima-u.ac.jp>
2024-06-21 10:21:34 +0900

    ユーザー編集基本画面の作成

--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -1,7 +1,25 @@
-import type { CreateUserForm } from '@/types/user_types';
+import type { CreateUserForm, UpdateUserForm } from '@/types/user_types';
 
 export const createUser = async (user: CreateUserForm) => {
   const response = await fetch(`http://localhost:8080/api/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (response.ok) {
+    return { success: true };
+  }
+  return {
+    success: false,
+    errors: await response.json().then((data) => data.errors),
+  };
+};
+
+export const editUser = async (user: UpdateUserForm) => {
+  const response = await fetch(`http://localhost:8080/api/edit`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
--- src/types/user_types.ts
+++ src/types/user_types.ts
@@ -29,3 +29,21 @@ export type CreateUserForm = z.infer<typeof CreateUserFormSchema>;
 export type CreateUserFormError = z.inferFormattedError<
   typeof CreateUserFormSchema
 >;
+
+export const UpdateUserFormSchema = z.object({
+  firstName: z.string().min(1, { message: '名前を入力してください' }),
+  lastName: z.string().min(1, { message: '姓を入力してください' }),
+  email: z
+    .string()
+    .email({ message: 'メールアドレスの形式ではありません' })
+    .min(1, { message: 'メールアドレスを入力してください' }),
+  zipcode: z.string().min(1, { message: '郵便番号を入力してください' }),
+  prefecture: z.string().min(1, { message: '都道府県を入力してください' }),
+  municipalities: z.string().min(1, { message: '市区町村を入力してください' }),
+  address: z.string().min(1, { message: '住所を入力してください' }),
+});
+
+export type UpdateUserForm = z.infer<typeof UpdateUserFormSchema>;
+export type UpdateUserFormError = z.inferFormattedError<
+  typeof UpdateUserFormSchema
+>;
