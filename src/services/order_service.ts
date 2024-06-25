3fe53b8f22f0f918737875914c842138e38e7b7a
char5742 <fujino@stellarcode.jp>
2024-06-25 18:03:24 +0900

    feat; order時に認証を行うように

--- src/services/order_service.ts
+++ src/services/order_service.ts
@@ -1,17 +1,22 @@
+import { fetchWithToken } from '@/lib/fetch';
 import { type CreateOrderForm } from '@/types/order_types';
 
 export const finalizeOrder = async (
-  userId: string,
+  token: string,
   form: CreateOrderForm,
   orderItemIdList: string[]
 ): Promise<void> => {
-  const response = await fetch(`http://localhost:8080/api/confirm/finalize`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ form: { userId, ...form, orderItemIdList } }),
-  });
+  const response = await fetchWithToken(
+    `http://localhost:8080/api/confirm/finalize`,
+    token,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ form: { ...form, orderItemIdList } }),
+    }
+  );
 
   if (!response.ok) {
     throw new Error(await response.text());
