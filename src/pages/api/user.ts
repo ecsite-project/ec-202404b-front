26b7f00cdfeadb8a0214b26b8a6818f661f65f7c
char5742 <fujino@stellarcode.jp>
2024-06-25 14:48:34 +0900

    feat: user情報を取得できるように

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,5 +1,11 @@
+import { fetchUserInfo } from '@/services/user_service';
 import type { APIRoute } from 'astro';
 
 export const GET: APIRoute = async ({ request, locals, redirect }) => {
-  locals.user;
+  const userInfo = await fetchUserInfo(locals);
+  return new Response(JSON.stringify(userInfo), {
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
 };
